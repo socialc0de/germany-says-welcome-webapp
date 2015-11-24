bff4238a465741bd1e573e12f7f877a27cb3f4af
pajowu <github@ca.pajowu.de>
2015-11-24 20:19:56 +0200

    implemented FAQ

--- js/app/data/Routes.js
+++ js/app/data/Routes.js
@@ -10,6 +10,11 @@ define({
     phrasebook_open_cat: 'phrasebook/{cat}',
     phrasebook_open_phrase: 'phrasebook/{cat}/{phrase}',
 
+    /* FAQ */
+    faq: 'faq',
+    faq_open_cat: 'faq/{cat}',
+    faq_open_item: 'faq/{cat}/{item}',
+
     /* Settings */
     settings: 'settings',
     settings_section: 'settings/{section}'
